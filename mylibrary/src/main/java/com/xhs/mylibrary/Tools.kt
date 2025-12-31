1510e76d534eae23916e19b42b8e914ae55959d5
ximei <ximei@android.com>
2025-12-31 15:40:35 +0800

    dev test

--- mylibrary/src/main/java/com/xhs/mylibrary/Tools.kt
+++ mylibrary/src/main/java/com/xhs/mylibrary/Tools.kt
@@ -9,4 +9,5 @@ object Tools {
 fun Int.calculate(another: Int ) = this + another
 fun String.form() = "$this 我是库文件的方法"
 fun String.add() = "$this 我是新添加的方法"
+fun String.dev() = "$this 我是分支dev添加的方法"
 fun String.toast(context: Context) = Toast.makeText(context,this, Toast.LENGTH_SHORT).show()
